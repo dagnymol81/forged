4f4fa1c649cb4a4fcd32e7c0c2c0c3f5b30a82aa
Dagny Mol <dagnymol@gmail.com>
2022-11-12 11:21:04 -0500

    ability modal

--- src/components/SelectAbility.js
+++ src/components/SelectAbility.js
@@ -37,32 +37,34 @@ export default function SelectAbility({ character, setCharacter }) {
   return(
     <>
 
-    <button onClick={addCustomAbility}>Add Custom Ability</button>
-
-    {abilities.map((box, i) => {
-      return(
-        <textarea 
-          key={i}
-          name={i}
-          defaultValue={abilities[i]} 
-          onChange={updateOneAbility}
-        />
-      )
-    })}
-
+    
   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Launch demo modal
+    Abilities
   </button>
 
 
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-xl">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Abilities</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
+
+      <button onClick={addCustomAbility}>Add Custom Ability</button>
+
+{abilities.map((box, i) => {
+  return(
+    <textarea 
+      key={i}
+      name={i}
+      defaultValue={abilities[i]} 
+      onChange={updateOneAbility}
+    />
+  )
+})}
+
 
       {startingAbilities.map((ability) => {
       return(
@@ -83,7 +85,6 @@ export default function SelectAbility({ character, setCharacter }) {
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
